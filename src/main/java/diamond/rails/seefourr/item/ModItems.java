492b6cc917b9c505f804a1b3a120aa373f5f6ae6
seefourr <106939690+seefourr@users.noreply.github.com>
2024-08-27 16:37:30 +1000

    remove unused import

--- src/main/java/diamond/rails/seefourr/item/ModItems.java
+++ src/main/java/diamond/rails/seefourr/item/ModItems.java
@@ -2,7 +2,6 @@ package diamond.rails.seefourr.item;
 
 import diamond.rails.seefourr.Diamondrails;
 import diamond.rails.seefourr.block.ModBlocks;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
@@ -11,20 +10,20 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     
-    public static final Item DIAMONDRAIL = registerItem("diamond_rail",
-            new BlockItem(ModBlocks.DIAMOND_RAIL,new FabricItemSettings()));
+    public static final Item DIAMOND_RAIL = registerItem("diamond_rail",
+            new BlockItem(ModBlocks.DIAMONDRAIL,new FabricItemSettings()));
     
-    public static final Item ENHANCEDDIAMONDRAIL = registerItem("enhanced_diamond_rail",
-            new BlockItem(ModBlocks.ENHANCED_DIAMOND_RAIL,new FabricItemSettings()));
+    public static final Item ENHANCED_DIAMOND_RAIL = registerItem("enhanced_diamond_rail",
+            new BlockItemWithGlint(ModBlocks.ENHANCEDDIAMONDRAIL,new FabricItemSettings()));
     
     public static final Item NETHERITE_RAIL = registerItem("netherite_rail",
-            new BlockItem(ModBlocks.NETHERITE_RAIL,new  FabricItemSettings()));
+            new BlockItem(ModBlocks.NETHERITERAIL,new  FabricItemSettings()));
 
     private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, new Identifier(DiamondRails.MOD_ID, name), item);
+        return Registry.register(Registries.ITEM, new Identifier(Diamondrails.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
-        DiamondRails.LOGGER.info("Registering Mod Items for " + DiamondRails.MOD_ID);
+        Diamondrails.LOGGER.info("Registering Mod Items for " + Diamondrails.MOD_ID);
     }
 }
