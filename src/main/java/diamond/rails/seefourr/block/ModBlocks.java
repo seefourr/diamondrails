397b82ed14f409c529bb58efd067864b361b4f52
seefourr <106939690+seefourr@users.noreply.github.com>
2024-08-27 16:36:53 +1000

    i forgor the class

--- src/main/java/diamond/rails/seefourr/block/ModBlocks.java
+++ src/main/java/diamond/rails/seefourr/block/ModBlocks.java
@@ -1,7 +1,6 @@
 package diamond.rails.seefourr.block;
 
 import diamond.rails.seefourr.Diamondrails;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.PoweredRailBlock;
@@ -14,25 +13,27 @@ import net.minecraft.util.Identifier;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 
-    public static final Block DIAMONDRAIL = Registry.register(Registries.BLOCK, new Identifier(DiamondRails.MOD_ID, "diamond_rail"),
+public class ModBlocks {
+
+    public static final Block DIAMONDRAIL = Registry.register(Registries.BLOCK, new Identifier(Diamondrails.MOD_ID, "diamond_rail"),
             new PoweredRailBlock(FabricBlockSettings.copyOf(Blocks.RAIL).sounds(BlockSoundGroup.METAL).strength(3.0f).noCollision()));
-    public static final Block ENHANCEDDIAMONDRAIL = Registry.register(Registries.BLOCK, new Identifier(DiamondRails.MOD_ID, "enhanced_diamond_rail"),
+    public static final Block ENHANCEDDIAMONDRAIL = Registry.register(Registries.BLOCK, new Identifier(Diamondrails.MOD_ID, "enhanced_diamond_rail"),
             new PoweredRailBlock(FabricBlockSettings.copyOf(Blocks.RAIL).sounds(BlockSoundGroup.METAL).strength(5.0f).noCollision()));
-    public static final Block NETHERITERAIL = Registry.register(Registries.BLOCK, new Identifier(DiamondRails.MOD_ID, "netherite_rail"),
+    public static final Block NETHERITERAIL = Registry.register(Registries.BLOCK, new Identifier(Diamondrails.MOD_ID, "netherite_rail"),
             new PoweredRailBlock(FabricBlockSettings.copyOf(Blocks.RAIL).sounds(BlockSoundGroup.METAL).strength(15.0f).noCollision()));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
-        return Registry.register(Registries.BLOCK, new Identifier(DiamondRails.MOD_ID, name), block);
+        return Registry.register(Registries.BLOCK, new Identifier(Diamondrails.MOD_ID, name), block);
     }
 
     private static Item registerBlockItem(String name, Block block) {
-        Item item = Registry.register(Registries.ITEM, new Identifier(DiamondRails.MOD_ID, name),
+        Item item = Registry.register(Registries.ITEM, new Identifier(Diamondrails.MOD_ID, name),
                 new BlockItem(block, new FabricItemSettings()));
         return item;
     }
 
     public static void registerModBlocks() {
-        DiamondRails.LOGGER.info("Registering Mod Blocks for " + DiamondRails.MOD_ID);
+        Diamondrails.LOGGER.info("Registering Mod Blocks for " + Diamondrails.MOD_ID);
     }
 }
